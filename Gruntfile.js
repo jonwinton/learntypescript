d1dc5ba6a7bf4d4b6da0d2664d5199c56d7c832e
Jon Winton <winton.jon@gmail.com>
2015-07-30 11:42:12 -0400

    Adding basic working example

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,20 @@
 module.exports = function(grunt) {
-  'use strict';
-  
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        ts: {
+            lib: {
+                files: [{
+                    src: ['./src/scripts/**/*.ts'],
+                    dest: './web/scripts'
+                }],
+                options: {fast: 'never'}
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-ts");
+    grunt.registerTask("default", ["ts"]);
 
 
-  // Default task(s).
-  grunt.registerTask('default', []);
 };
---
+++ web/scripts/main.js
@@ -0,0 +1,3 @@
+'use strict';
+console.log('HELLO');
+//# sourceMappingURL=main.js.map
