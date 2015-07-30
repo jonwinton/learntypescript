c8816e6a457df79354a0669001deea91b1d4fd14
Jon Winton <winton.jon@gmail.com>
2015-07-30 11:07:39 -0400

    Adding base files

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+  'use strict';
+  
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json')
+  });
+
+
+  // Default task(s).
+  grunt.registerTask('default', []);
+};
